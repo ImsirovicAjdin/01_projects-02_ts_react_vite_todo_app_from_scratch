20b3663eaded7b8c94df60839139f9f3feea594b
Ajdin Imsirovic <ajdika@live.com>
2024-09-29 20:31:45 +0200

    002a, Initialize inputValue state to hold input value; 002b Bind the input field's value to the component's state to reflect user input

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@
 import React from "react";
 
 const App: React.FC = () => {
+  const [inputValue, setInputValue] = React.useState<string>(""); // (002a)
+
   return (
     <div>
       <h1>My Todo App</h1>
-      <input type="text" placeholder="Add a new todo" />
+      <input
+        type="text"
+        placeholder="Add a new todo"
+        value={inputValue} // (002b)
+      />
       <button>Add Todo</button>
 
       <ul>
